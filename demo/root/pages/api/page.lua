3deb79f7ccac6f7117581838480372c8662351f6
霊音・永旻 <reineimi.dev@gmail.com>
2024-11-05 15:37:27 +0400

    Add files via upload

--- demo/root/pages/api/page.lua
+++ demo/root/pages/api/page.lua
@@ -1,6 +1,7 @@
 local init = require 'init'
 local body = [[
-<div class='i'>
+<div class='i center' style='gap: 1rem'>
+	<p class='w'><a href='https://github.com/reineimi/va2-ngx-test' target='_blank'>This project's GitHub repository</a></p>
 	<p class='w'><a href='https://openresty-reference.readthedocs.io/en/latest/Lua_Nginx_API/' target='_blank'>API Reference</a></p>
 	<p class='w'><a href='https://programming-language-benchmarks.vercel.app/lua-vs-php' target='_blank'>Lua vs PHP Benchmarks</a></p>
 </div>
